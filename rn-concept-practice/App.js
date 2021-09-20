ec1d63a14696651f80e17b05c94ce89d7e1da894
“Ron <wheresronald0@icloud.com>
2021-09-20 16:36:50 -0400

    added button and text imput with basic in-line styling

--- rn-concept-practice/App.js
+++ rn-concept-practice/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+     <View>
+       <TextInput 
+       placeholder="Enter Goal Here" 
+       style={{ borderBottomColor: 'blue', borderBottomWidth: 1, padding: 10}}/>
+       <Button title="Add"/>
+     </View> 
+     <View>
+       
+     </View>
       <StatusBar style="auto" />
     </View>
   );
