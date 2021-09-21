be9e5e5f5c7dc8c605641ea88fd694674bde8c4c
“Ron <wheresronald0@icloud.com>
2021-09-21 13:33:30 -0400

    using stylesheet.create now

--- rn-concept-practice/App.js
+++ rn-concept-practice/App.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-     <View>
+    <View style={styles.screen}>
+     <View style={styles.inputContainer}>
        <TextInput 
        placeholder="Enter Goal Here" 
-       style={{ borderBottomColor: 'blue', borderBottomWidth: 1, padding: 10}}/>
+       style={styles.input}/>
        <Button title="Add"/>
      </View> 
      <View>
@@ -20,10 +20,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+  screen: {
+    padding: 50,
+    backgroundColor: 'grey',
   },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    width: 200, 
+    borderBottomColor: 'blue', 
+    borderBottomWidth: 1, 
+    padding: 10
+  }
 });
